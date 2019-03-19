6f75382f66f62d89d0741e83ee1bdaad7c598501
Oleksandr Davydenko <47133869+oleksander87@users.noreply.github.com>
2019-03-19 16:40:35 +0200

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,33 @@
+
+//-------------Davydenko-Oleksandr-FEA-12-------------------------------------------
+//---------------------HOME WORK 6--------------------------------------------------
+//-----------------------HW 6.1-----------------------------------------------------
+//----------------------------------------------------------------------------------
+var tags = ["h1","h2","h3","h4"];
+
+var classes = ["mouse","botton","scrol","sensor"];
+
+var style = document.head.appendChild(document.createElement('style'));
+
+style.textContent = `.mouse,.botton,.scrol,.sensor{
+    width: 300px;
+    height: 300px;
+  } 
+  .mouse {
+    background-color: #11114c;
+  }
+  .botton {
+    background-color: #feff2c;
+  }
+  .scrol {
+    background-color: #962929;
+  }
+  .sensor {
+    background-color: #ff0000;
+  }`
+
+for( var indEx in tags) {
+  var elem = document.createElement(tags[indEx]);
+  elem.classList.add(classes[indEx]);
+  document.body.appendChild(elem);
+}
